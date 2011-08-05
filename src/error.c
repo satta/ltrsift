4f3b06298bf530f3cda7dbd965dd16e879205de7
Sascha Kastens <mail@skastens.de>
2011-08-05 16:33:19 +0200

    basic error handling

---
+++ src/error.c
@@ -0,0 +1,20 @@
+#include "error.h"
+
+void error_handle(GError *err)
+{
+  if (err != NULL) {
+    GtkWidget *dialog;
+
+    /* log to terminal stderr */
+    fprintf(stderr, "ERROR: %s\n", err->message);
+
+    /* create an error message dialog and display modally to the user */
+    dialog = gtk_message_dialog_new(NULL,
+                                    GTK_DIALOG_MODAL |
+                                                 GTK_DIALOG_DESTROY_WITH_PARENT,
+                                    GTK_MESSAGE_ERROR, GTK_BUTTONS_OK, "test");
+    gtk_window_set_title(GTK_WINDOW(dialog), "ERROR!");
+    gtk_dialog_run(GTK_DIALOG(dialog));
+    gtk_widget_destroy(dialog);
+  }
+}
---
+++ src/error.h
@@ -0,0 +1,8 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+#include <gtk/gtk.h>
+
+void error_handle(GError *err);
+
+#endif // ERROR_H
