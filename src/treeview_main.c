5b86c39de9e2df7890bbb6b4dedb778c8d825449
Sascha Kastens <sascha.kastens@studium.uni-hamburg.de>
2011-08-30 17:59:18 +0200

    treeview_main shows child data on demand, i.e. when the user clicks the arrow. data will be dropped in case the row is collapsed

--- src/treeview_main.c
+++ src/treeview_main.c
@@ -1,15 +1,14 @@
 #include "treeview_main.h"
 #include "unused.h"
 
-static int add_feature_columns(void *key, void *value, void *ltrgui, GT_UNUSED GtError *err)
+static int add_feature_columns(void *key, void *value, void *ltrgui,
+                               GT_UNUSED GtError *err)
 {
   GtkCellRenderer *renderer = gtk_cell_renderer_text_new();
   GtkTreeViewColumn *column;
   const char *caption = (const char*) key;
   unsigned long num = (unsigned long) value;
   GUIData *gui = (GUIData*) ltrgui;
-
-//  g_warning("num: %lu", num);
 
   column = gtk_tree_view_column_new_with_attributes(caption, renderer, "text",
                                                     num, NULL);
@@ -18,12 +17,96 @@ static int add_feature_columns(void *key, void *value, void *ltrgui, GT_UNUSED G
   return 0;
 }
 
-void tv_main_init(GUIData *ltrgui, GtArray *nodes, GtHashmap *features,
-                  unsigned long noc)
+static void tw_main_append_child(GUIData *ltrgui,
+                          GtkTreeModel *model,
+                          GtkTreeIter *child,
+                          GtkTreeIter *iter,
+                          GtFeatureNode *curnode,
+                          const char *type,
+                          const char *fnt)
+{
+  const char *clid;/*, *seqid;*/
+  unsigned long cno;
+  GtRange range;
+/*seqid = gt_str_get(gt_genome_node_get_seqid((GtGenomeNode*) curnode));*/
+  range = gt_genome_node_get_range((GtGenomeNode*) curnode);
+  clid = gt_feature_node_get_attribute(curnode, ATTR_CLUSTID);
+  cno = (unsigned long) gt_hashmap_get(ltrgui->features, type);
+  gtk_tree_store_append(GTK_TREE_STORE(model), child, iter);
+  gtk_tree_store_set(GTK_TREE_STORE(model), child,
+                   /*  TV_MAIN_SEQID, seqid,*/
+                     TV_MAIN_TYPE, fnt,
+                     TV_MAIN_START, range.start,
+                     TV_MAIN_END, range.end,
+                     TV_MAIN_EMPTY, "|",
+                     cno, clid,
+                     -1);
+}
+
+
+void tw_main_row_collapsed(GtkTreeView *tree_view, GtkTreeIter *iter,
+                          G_UNUSED GtkTreePath *path, G_UNUSED GUIData *ltrgui)
+{
+  GtkTreeIter child;
+  GtkTreeModel *model;
+
+  model = gtk_tree_view_get_model(tree_view);
+  gtk_tree_model_iter_children(model, &child, iter);
+  while (gtk_tree_store_remove(GTK_TREE_STORE(model), &child)) {
+    continue;
+  }
+  gtk_tree_store_append(GTK_TREE_STORE(model), &child, iter);
+}
+
+void tw_main_row_expanded(GtkTreeView *tree_view, GtkTreeIter *iter,
+                          GtkTreePath *path, G_UNUSED GUIData *ltrgui)
+{
+  GtkTreeModel *model;
+  GtkTreeIter child, tmp1, tmp2;
+  GtGenomeNode **gn;
+  GtFeatureNodeIterator *fni;
+  GtFeatureNode *curnode;
+  const char *fnt, *tmp = NULL;
+  gboolean first_ltr = true;
+
+  model = gtk_tree_view_get_model(tree_view);
+  /* get iter for first row, will be needed to remove empty row (see below) */
+  gtk_tree_model_get_iter(model, &tmp1, path);
+  /* get GtGenomeNode */
+  gtk_tree_model_get(model, iter,
+                     TV_MAIN_NODE, &gn, -1);
+  fni = gt_feature_node_iterator_new((GtFeatureNode*) gn);
+
+  while ((curnode = gt_feature_node_iterator_next(fni))) {
+    fnt = gt_feature_node_get_type(curnode);
+    if (g_strcmp0(fnt, FNT_PROTEINM) == 0) {
+      const char *attr = gt_feature_node_get_attribute(curnode, ATTR_PFAMN);
+      if (gt_hashmap_get(ltrgui->features, attr) != NULL)
+        tw_main_append_child(ltrgui, model, &child, iter, curnode, attr, fnt);
+    } else if (g_strcmp0(fnt, FNT_LTR) == 0) {
+      if (first_ltr) {
+        tmp = FNT_LLTR;
+        first_ltr = false;
+      } else
+        tmp = FNT_RLTR;
+      if (gt_hashmap_get(ltrgui->features, tmp) != NULL)
+        tw_main_append_child(ltrgui, model, &child, iter, curnode, tmp, fnt);
+    } else {
+      if (gt_hashmap_get(ltrgui->features, fnt) != NULL)
+        tw_main_append_child(ltrgui, model, &child, iter, curnode, fnt, fnt);
+    }
+  }
+  gt_feature_node_iterator_delete(fni);
+  /* remove empty row */
+  gtk_tree_model_iter_children(model, &tmp2, &tmp1);
+  gtk_tree_store_remove(GTK_TREE_STORE(model), &tmp2);
+}
+
+void tv_main_init(GUIData *ltrgui, GtArray *nodes, unsigned long noc)
 {
   GtkCellRenderer *renderer;
   GtkTreeViewColumn *column;
-  GtkTreeIter iter;
+  GtkTreeIter iter, child;
   GtkTreeStore *store;
   GType *types;
   GtError *err = NULL;
@@ -57,15 +140,24 @@ void tv_main_init(GUIData *ltrgui, GtArray *nodes, GtHashmap *features,
                                                     TV_MAIN_END, NULL);
   gtk_tree_view_append_column(GTK_TREE_VIEW(ltrgui->tv_main), column);
 
-  gt_hashmap_foreach(features, add_feature_columns, (void*) ltrgui, err);
+  renderer = gtk_cell_renderer_text_new();
+  column = gtk_tree_view_column_new_with_attributes("",
+                                                    renderer, "text",
+                                                    TV_MAIN_EMPTY, NULL);
+  gtk_tree_view_append_column(GTK_TREE_VIEW(ltrgui->tv_main), column);
 
-  types[0] = G_TYPE_STRING;
+  gt_hashmap_foreach(ltrgui->features, add_feature_columns, (void*) ltrgui,
+                     err);
+
+  types[0] = G_TYPE_POINTER;
   types[1] = G_TYPE_STRING;
-  types[2] = G_TYPE_ULONG;
+  types[2] = G_TYPE_STRING;
   types[3] = G_TYPE_ULONG;
+  types[4] = G_TYPE_ULONG;
+  types[5] = G_TYPE_STRING;
 
   for (i = TV_MAIN_N_COLUMS; i < noc; i++) {
-    types[i] = G_TYPE_UINT;
+    types[i] = G_TYPE_STRING;
   }
 
   store = gtk_tree_store_newv(noc, types);
@@ -75,56 +167,79 @@ void tv_main_init(GUIData *ltrgui, GtArray *nodes, GtHashmap *features,
     GtFeatureNode *curnode;
     GtFeatureNodeIterator *fni;
     const char *fnt;
+    gboolean first_ltr = true;
     gn = *(GtGenomeNode***) gt_array_get(nodes, i);
 
     fni = gt_feature_node_iterator_new((GtFeatureNode*) gn);
 
     while ((curnode = gt_feature_node_iterator_next(fni))) {
       fnt = gt_feature_node_get_type(curnode);
-      if ((g_strcmp0(fnt, "repeat_region") == 0)) {
+      if ((g_strcmp0(fnt, FNT_REPEATR) == 0)) {
         char *seqid;
         GtRange range;
         seqid = gt_str_get(gt_genome_node_get_seqid((GtGenomeNode*) curnode));
         range = gt_genome_node_get_range((GtGenomeNode*) curnode);
         gtk_tree_store_append(store, &iter, NULL);
         gtk_tree_store_set(store, &iter,
+                           TV_MAIN_NODE, gn,
                            TV_MAIN_SEQID, seqid,
-                           TV_MAIN_TYPE, "",
                            TV_MAIN_START, range.start,
                            TV_MAIN_END, range.end,
+                           TV_MAIN_EMPTY, "|",
                            -1);
-      } else if (g_strcmp0(fnt, "protein_match") == 0) {
-        const char *attr = gt_feature_node_get_attribute(curnode, "pfamname");
+        /* append an empty row for row-expand functionality */
+        gtk_tree_store_append(store, &child, &iter);
+      } else if (g_strcmp0(fnt, FNT_PROTEINM) == 0) {
+        const char *attr = gt_feature_node_get_attribute(curnode, ATTR_PFAMN);
 
-        if (gt_hashmap_get(features, attr) != NULL) {
+        if (gt_hashmap_get(ltrgui->features, attr) != NULL) {
           const char *clid;
-          unsigned long cno, cval;
-          clid = gt_feature_node_get_attribute(curnode, "clid");
-          sscanf(clid, "%lu", &cval);
-          cno = (unsigned long) gt_hashmap_get(features, attr);
+          unsigned long cno;
+          clid = gt_feature_node_get_attribute(curnode, ATTR_CLUSTID);
+          cno = (unsigned long) gt_hashmap_get(ltrgui->features, attr);
           gtk_tree_store_set(store, &iter,
-                             cno, cval,
+                             cno, clid,
                              -1);
         }
-      } else if ((gt_hashmap_get(features, fnt)) != NULL) {
+      } else if (g_strcmp0(fnt, FNT_LTR) == 0) {
+        const char *tmp;
+        if (first_ltr) {
+          tmp = FNT_LLTR;
+          first_ltr = false;
+        } else
+          tmp = FNT_RLTR;
+        if (gt_hashmap_get(ltrgui->features, tmp) != NULL) {
+          const char *clid;
+          unsigned long cno;
+          clid = gt_feature_node_get_attribute(curnode, ATTR_CLUSTID);
+          cno = (unsigned long) gt_hashmap_get(ltrgui->features, tmp);
+          gtk_tree_store_set(store, &iter,
+                             cno, clid,
+                             -1);
+        }
+      } else if ((gt_hashmap_get(ltrgui->features, fnt)) != NULL) {
         const char *clid;
-        unsigned long cno, cval;
-        clid = gt_feature_node_get_attribute(curnode, "clid");
-        sscanf(clid, "%lu", &cval);
-        cno = (unsigned long) gt_hashmap_get(features, fnt);
+        unsigned long cno;
+        clid = gt_feature_node_get_attribute(curnode, ATTR_CLUSTID);
+        cno = (unsigned long) gt_hashmap_get(ltrgui->features, fnt);
         gtk_tree_store_set(store, &iter,
-                           cno, cval,
+                           cno, clid,
                            -1);
       }
     }
     gt_feature_node_iterator_delete(fni);
   }
 
-  gtk_tree_view_set_model(GTK_TREE_VIEW(ltrgui->tv_main), GTK_TREE_MODEL(store));
+  gtk_tree_view_set_model(GTK_TREE_VIEW(ltrgui->tv_main),
+                          GTK_TREE_MODEL(store));
   gtk_tree_view_set_headers_clickable(GTK_TREE_VIEW(ltrgui->tv_main), TRUE);
   g_object_unref(store);
 
   gtk_container_add(GTK_CONTAINER(ltrgui->sw_main), ltrgui->tv_main);
+  g_signal_connect(G_OBJECT(ltrgui->tv_main), "row-expanded",
+                   G_CALLBACK(tw_main_row_expanded), ltrgui);
+  g_signal_connect(G_OBJECT(ltrgui->tv_main), "row-collapsed",
+                   G_CALLBACK(tw_main_row_collapsed), ltrgui);
   gtk_widget_show(ltrgui->tv_main);
 
   g_free(types);
--- src/treeview_main.h
+++ src/treeview_main.h
@@ -8,16 +8,30 @@
 #define TV_MAIN_CAPTION_START "Start"
 #define TV_MAIN_CAPTION_END   "End"
 #define TV_MAIN_CAPTION_CLUSTER "cluster no."
+#define ATTR_CLUSTID "clid"
+#define ATTR_PFAMN   "pfamname"
+#define FNT_PROTEINM "protein_match"
+#define FNT_LTR      "long_terminal_repeat"
+#define FNT_REPEATR  "repeat_region"
+#define FNT_LLTR     "lLTR"
+#define FNT_RLTR     "rLTR"
 
 typedef enum {
-  TV_MAIN_SEQID = 0,
+  TV_MAIN_NODE = 0,
+  TV_MAIN_SEQID,
   TV_MAIN_TYPE,
   TV_MAIN_START,
   TV_MAIN_END,
+  TV_MAIN_EMPTY,
   TV_MAIN_N_COLUMS
 } TreeViewMainColumns;
 
-void tv_main_init(GUIData *ltrgui, GtArray *nodes, GtHashmap *features,
-                  unsigned long nof_features);
+void tv_main_init(GUIData *ltrgui, GtArray *nodes, unsigned long nof_features);
+
+void tw_main_row_collapsed(GtkTreeView *tree_view, GtkTreeIter *iter,
+                           GtkTreePath *path, GUIData *ltrgui);
+
+void tw_main_row_expanded(GtkTreeView *tree_view, GtkTreeIter *iter,
+                          GtkTreePath *path, GUIData *ltrgui);
 
 #endif // TREEVIEW_MAIN_H
