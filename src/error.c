790669a680857eb7758426d3fca4a63e0cac0f63
Sascha Steinbiss <steinbiss@zbh.uni-hamburg.de>
2012-06-26 18:53:27 +0200

    clean up error messages

--- src/error.c
+++ src/error.c
@@ -17,7 +17,10 @@
 
 #include "error.h"
 
-void _error_handle(GtkWidget *toplevel, GtError *err, const char *func, const char *file, int line)
+void _error_handle(GtkWidget *toplevel, GtError *err,
+                   GT_UNUSED const char *func,
+                   GT_UNUSED const char *file,
+                   GT_UNUSED int line)
 {
   if ((err != NULL) && gt_error_is_set(err)) {
     GtkWidget *dialog;
@@ -27,8 +30,8 @@ void _error_handle(GtkWidget *toplevel, GtError *err, const char *func, const ch
                                     GTK_DIALOG_MODAL |
                                     GTK_DIALOG_DESTROY_WITH_PARENT,
                                     GTK_MESSAGE_ERROR, GTK_BUTTONS_OK,
-                                    "%s %s %s:%d",
-                                    gt_error_get(err), func, file, line);
+                                    "%s",
+                                    gt_error_get(err));
     gtk_window_set_title(GTK_WINDOW(dialog), "Error");
     gtk_dialog_run(GTK_DIALOG(dialog));
     gtk_widget_destroy(dialog);
