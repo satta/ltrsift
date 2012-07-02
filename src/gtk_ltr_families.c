ddfa95666a2ec5461a0b9643369f280d45faee44
Sascha Steinbiss <steinbiss@zbh.uni-hamburg.de>
2012-07-02 19:04:11 +0200

    fixup style

--- src/gtk_ltr_families.c
+++ src/gtk_ltr_families.c
@@ -4440,7 +4440,7 @@ void gtk_ltr_families_fill_with_data(GtkLTRFamilies *ltrfams,
   ltrfams->n_features = noc;
   ltrfams->style = gt_style_new(ltrfams->err);
 
-  if (gt_file_exists((const char*) ltrfams->style_file))
+  if (gt_file_exists((const char*) ltrfams->style_file)) {
     had_err = gt_style_load_file(ltrfams->style, ltrfams->style_file,
                                  ltrfams->err);
     if (had_err)
